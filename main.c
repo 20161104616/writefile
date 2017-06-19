ea5d6c1cb023a67d3b6e33717988e0da97a63725
20161104616 <992846873@qq.com>
2017-06-19 12:00:20 +0800

    Buzhidaozhenmele

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    //FILE* fp =fopen("1.txt", "w");
+    scanf("%d",&n);
+    printf("%d",n);
+   // fprintf(fp, "写入一段话，%d",n);
+    //fclose(fp);
+    return 1;
+}
